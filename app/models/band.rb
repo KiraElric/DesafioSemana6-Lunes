217a2dcc557f8326b881b4bde61c4dd0bb357a19
KiraElric <lissette.urso@gmail.com>
2021-03-22 00:34:23 -0300

    Creacción del seed para poblar la base de datos

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,3 +1,4 @@
 class Band < ApplicationRecord
   has_many :concerts, dependent: :destroy
+  enum kind: [:band, :soloist]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Creación de data para la bd a traves de la gema faker
+Band.destroy_all
+
+50.times do |i|
+  band = Band.create(name: Faker::Music.band, members: Random.rand(1...10),
+                    debut_date: Date.today + (i+2).days - (i+3).months - (i+4).years,
+                    kind: Band.kinds.keys.sample)
+  puts "Se creo la banda: #{band.name}"
+  10.times do |i|
+    concert = band.concerts.build(attendance: Random.rand(1000...50000), duration: Random.rand(90...240),
+                                  date: Date.today + (i+1).days - (i+2).months - (i).years )
+    concert.save
+    puts "Se realizó el concierto: #{concert.date}"
+  end
+end
