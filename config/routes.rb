67ff39259fc4fc1d19a152393473fbc1f1db8fe0
KiraElric <lissette.urso@gmail.com>
2021-03-21 23:27:55 -0300

    Anidacion de las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :concerts
-  resources :bands
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :bands do
+    resources :concerts
+  end
+
+  root 'bands#index'
 end
