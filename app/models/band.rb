36466d942efea0874ed2b3aae84ecf8019ea3670
KiraElric <lissette.urso@gmail.com>
2021-03-22 01:42:33 -0300

    Adicion de metodos en el modelo de band para visualizar lo solicitado en el show, repoblacion de la bd por cambio de seed

--- app/models/band.rb
+++ app/models/band.rb
@@ -6,11 +6,33 @@ class Band < ApplicationRecord
     name
   end
 
+  #Metodo para conocer la cantidad de conciertos por banda
   def concert_count
     self.concerts.count
   end
 
+  #Metodo para conocer el total de audiencia de todos los conciertos de una banda
+  def total_attendence
+    self.concerts.sum(:attendance)
+  end
+
+  #Metodo para conocer los conciertos realizados en el mes en curso
   def current_concert
     self.concerts.where('date between ? and ?', Date.today.beginning_of_month, Date.today.end_of_month).size
+  end
+
+  #Metodo para conocer el maximo de audiencia de un concierto de una banda
+  def max_attendance
+    self.concerts.pluck(:attendance).max
+  end
+
+  #Metodo para conocer la maxima duracion de un concierto de una banda
+  def max_duration
+    self.concerts.maximum(:duration)
+  end
+
+  #Metodo para conocer el ultimo concierto de una banda con la fecha escrita en palabras
+  def last_concert
+    self.concerts.maximum(:date).strftime("%Y - %B - %A")
   end
 end
